6c0236a67c3467b9f667c645d3e0425b4c17be47
William Wallace <wthered@gmail.com>
2020-12-10 19:34:06 +0200

    Last commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Login from "@/views/Login";
 import Register from "@/views/Register";
 import UserPage from "@/views/UserPage";
+import Film from "@/components/Film";
 
 Vue.use(VueRouter)
 
@@ -35,6 +36,11 @@ const routes = [
 		path: "/user/:user",
 		name: 'UserPage',
 		component: UserPage
+	},
+	{
+		path: '/film/:film',
+		name: 'Film',
+		component: Film
 	}
 ]
 
